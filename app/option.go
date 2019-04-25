03a6d8b2ec0cf52a1fbceaa029808b069fe769eb
zp_amd <287206570@qq.com>
2019-04-25 09:09:38 +0800

    还原至0.0.1版本

--- app/option.go
+++ app/option.go
@@ -4,7 +4,6 @@
 package app
 
 import (
-	"github.com/zpab123/sco/discovery"  // 服务发现
 	"github.com/zpab123/sco/netservice" // 网络服务
 )
 
@@ -13,24 +12,20 @@ import (
 
 // app 配置参数
 type Option struct {
-	NetServiceOpt     *netservice.TNetServiceOpt   // 网络服务参数
-	ClentMsgChanSize  int                          // 客户端消息通道长度
-	ServerMsgChanSize int                          // 服务器消息长度
-	Cluster           bool                         // 是否启动集群模式
-	DiscoveryOpt      *discovery.TEtcdDiscoveryOpt // 服务发现参数
+	NetServiceOpt     *netservice.TNetServiceOpt // 网络服务参数
+	ClentMsgChanSize  int                        // 客户端消息通道长度
+	ServerMsgChanSize int                        // 服务器消息长度
 }
 
 // 设置 app 的默认参数
 func setdDfaultOpt(app *Application) {
 	// 网络服务
 	nsOpt := netservice.NewTNetServiceOpt()
-	dcOpt := discovery.NewTEtcdDiscoveryOpt()
 
 	opt := &Option{
 		NetServiceOpt:     nsOpt,
 		ClentMsgChanSize:  C_CLIENT_MSG_CHAN_SIZE,
 		ServerMsgChanSize: C_SERVER_MSG_CHAN_SIZE,
-		DiscoveryOpt:      dcOpt,
 	}
 
 	app.Option = opt
