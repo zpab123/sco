191efa0662e3ab6ec761872ff316cd5fa3e6f66f
张鹏 <287206570@qq.com>
2020-05-08 18:37:44 +0800

    完成启动与退出

--- app/application.go
+++ app/application.go
@@ -39,15 +39,10 @@ func (this *Application) Run() {
 	// 设置随机种子
 	rand.Seed(time.Now().UnixNano())
 
+	zaplog.Infof("服务器，启动成功...")
+
 	// 侦听结束信号
 	this.waitStopSignal()
-
-	zaplog.Infof("服务器启动成功")
-
-	for {
-		zaplog.Infof("服务器运行中...")
-		time.Sleep(3 * time.Second)
-	}
 }
 
 // 停止 app
@@ -60,21 +55,20 @@ func (this *Application) Stop() {
 func (this *Application) waitStopSignal() {
 	// 排除信号
 	signal.Ignore(syscall.Signal(10), syscall.Signal(12), syscall.SIGPIPE, syscall.SIGHUP)
-	signal.Notify(this.signalChan, syscall.SIGILL, syscall.SIGTERM)
+	signal.Notify(this.signalChan, syscall.SIGINT, syscall.SIGTERM)
 
-	go func() {
-		for {
-			sig := <-this.signalChan
-			if syscall.SIGINT == sig || syscall.SIGTERM == sig {
-				zaplog.Infof("服务器，正在停止中，请等待 ...")
+	for {
+		sig := <-this.signalChan
+		if syscall.SIGINT == sig || syscall.SIGTERM == sig {
+			zaplog.Infof("服务器，正在停止中，请等待 ...")
+			this.Stop()
 
-				time.Sleep(C_STOP_OUT_TIME)
+			time.Sleep(C_STOP_OUT_TIME)
 
-				zaplog.Warnf("服务器，超过 %v 秒未优雅关闭，强制关闭", C_STOP_OUT_TIME)
-				os.Exit(1)
-			} else {
-				zaplog.Warnf("异常的操作系统信号=%s", sig)
-			}
+			zaplog.Warnf("服务器，超过 %v 秒未优雅关闭，强制关闭", C_STOP_OUT_TIME)
+			os.Exit(1)
+		} else {
+			zaplog.Warnf("异常的操作系统信号=%s", sig)
 		}
-	}()
+	}
 }
