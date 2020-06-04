67608b046f063ab556217a0db0ca619906e23a3a
张鹏 <287206570@qq.com>
2020-06-04 12:09:04 +0800

    优化连接

--- network/conn_mgr.go
+++ network/conn_mgr.go
@@ -5,6 +5,7 @@ package network
 
 import (
 	"github.com/zpab123/syncutil"
+	"github.com/zpab123/zaplog"
 	"golang.org/x/net/websocket"
 )
 
@@ -37,5 +38,28 @@ func (this *ConnMgr) Stop() {
 
 // 收到1个新的 websocket 连接对象 [IWsConnManager]
 func (this *ConnMgr) OnNewWsConn(wsconn *websocket.Conn) {
+	// 超过最大连接数
+	if this.connNum.Load() >= this.maxConn {
+		wsconn.Close()
+		zaplog.Warnf("[ConnMgr] 达到最大连接数，关闭新连接。当前连接数=%d", this.connNum.Load())
+	}
 
+	// 参数设置
+	wsconn.PayloadType = websocket.BinaryFrame // 以二进制方式接受数据
+
+	// 创建代理
+	zaplog.Debugf("[ConnMgr] 新 ws 连接，ip=%s", wsconn.RemoteAddr())
+	this.newAgent(wsconn, true)
+}
+
+// 创建代理
+func (this *ConnMgr) newAgent(conn net.Conn, isWebSocket bool) {
+	/*
+		// 创建 socket
+		socket := Socket{
+			Conn: conn,
+		}
+
+		this.connNum.Add(1)
+	*/
 }
